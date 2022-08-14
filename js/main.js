1c208fbd7f14747aae47cee695f3ee4bd42e7b13
my73xmobilcode <my.73x.mobil.code@gmail.com>
2022-08-14 12:44:04 -0700

    init_commit

---
+++ js/main.js
@@ -0,0 +1,8 @@
+// HANDLE THE LOAD EVENT OF THE WINDOW
+
+// HANDLE THE CLICK EVENT FOR THE BTNADDEMPLOYEE BUTTON
+document.getElementById('btnAddEmployee').addEventListener('click', () => {
+    window.open('add-employee.html','add-employe','width=500, height=580, resizeable=false')
+})
+
+// OPEN THE ADD-EMPLOYEE.HTML PAGE WITHIN A POPUP
